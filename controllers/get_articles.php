2135921959e060bfc1399faac47704e19f80d776
Charbel <charbel.dawoud@gmail.com>
2025-06-26 18:48:01 +0200

    optimized find api

--- controllers/get_articles.php
+++ controllers/get_articles.php
@@ -5,44 +5,21 @@ require("../connection/connection.php");
 
 $response = [];
 $response["status"] = 200;
-$response["articles"] = [];
 
-if(isset($_GET["id"])){
-    $id = $_GET["id"];
+if(!isset($_GET["id"])){
+    $articles = Article::all($mysqli); //reminder: this is an array of OBJECTS!!!!
 
-    $article = Article::find($mysqli, $id);
-    $user = User::find($mysqli, 2);
-
-    /*$article = new Article();
-    $article->update($mysqli, $id);*/
-
-    $response["article"] = $article->toArray();
-    $response["user"] = $user->toArray();
-
-    echo json_encode($response);
+    $response["articles"] = []; //json_encode DOES NOT read private attributes!!!
+    foreach($articles as $a){
+        $response["articles"][] = $a->toArray(); //hence, we decided to iterate again on the articles array and now to store the result of the toArray() which is an array. 
+    }
+    echo json_encode($response); //now we can call json_encode on array of arrays. 
     return;
 }
 
+$id = $_GET["id"];
+$article = Article::find($mysqli, $id);
+$response["article"] = $article->toArray();
 
-
-
-
-
-
-
-
-
-
-
-$query = $mysqli->prepare("SELECT * from articles");
-$query->execute();
-
-$array = $query->get_result();
-
-$articles = []; //temp array to store the articles from the db
-while($article = $array->fetch_assoc()){
-    $articles[] = $article;
-}
-
-$response["articles"] = $articles;
 echo json_encode($response);
+return;
--- models/Model.php
+++ models/Model.php
@@ -5,7 +5,6 @@ abstract class Model{
     protected static string $primary_key = "id";
 
     public static function find(mysqli $mysqli, int $id){
-        
         $sql = sprintf("Select * from %s WHERE %s = ?", 
                         static::$table, 
                         static::$primary_key);
@@ -18,6 +17,23 @@ abstract class Model{
 
         return $data ? new static($data) : null;
     }
+
+    public static function all(mysqli $mysqli){
+        $sql = sprintf("Select * from %s", static::$table);
+        
+        $query = $mysqli->prepare($sql);
+        $query->execute();
+
+        $data = $query->get_result();
+
+        $objects = [];
+        while($row = $data->fetch_assoc()){
+            $objects[] = new static($row); //creating an object of type "static" / "parent" and adding the object to the array
+        }
+
+        return $objects; //we are returning an array of objects!!!!!!!!
+    }
+
 }
 
 
